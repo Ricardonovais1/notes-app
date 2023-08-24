3fd959c8f6962049270094f8388ad84ed7ef4f2e
Ricardonovais1 <ricardonovais1@gmail.com>
2023-08-24 19:52:46 -0300

    Reverter Action Cable

--- src/pages/NotesPage.js
+++ src/pages/NotesPage.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import ListItem from '../components/ListItem'
 import AddButton from '../components/AddButton'
-import { createConsumer } from 'actioncable'
 
 
 const NotesPage = () => {
@@ -9,33 +8,9 @@ const NotesPage = () => {
   let [notes, setNotes] = useState([]);
 
   useEffect(() => {
-<<<<<<< HEAD
     getNotes()
-=======
-    // getNotes()
->>>>>>> e8af2b6ec6cf580d871ed370d4423d0cab1400a0
-    getNotesChannel()
   }, [])
 
-  const getNotesChannel = () => {
-    const cable = createConsumer('https://rails-vxml.onrender.com/api/v1/notes/cable'); // Substitua pela URL correta
-
-    const noteChannel = cable.subscriptions.create(
-      { channel: 'NoteChannel' },
-      {
-        received: (data) => {
-          if (data.type === 'index') {
-            setNotes(data.notes);
-          } else if (data.type === 'update') {
-            const updatedNote = data.note;
-            setNotes((prevNotes) =>
-              prevNotes.map((note) => (note.id === updatedNote.id ? updatedNote : note))
-            );
-          }
-        },
-      }
-    );
-  }
 
   const getNotes = async () => {
     let response = await fetch('https://rails-vxml.onrender.com/api/v1/notes/')

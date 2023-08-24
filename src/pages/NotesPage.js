e8af2b6ec6cf580d871ed370d4423d0cab1400a0
Ricardonovais1 <ricardonovais1@gmail.com>
2023-08-24 16:57:04 -0300

    Action Cable

--- src/pages/NotesPage.js
+++ src/pages/NotesPage.js
@@ -1,15 +1,37 @@
 import React, { useState, useEffect } from 'react'
-// import notes from '../assets/data'
 import ListItem from '../components/ListItem'
 import AddButton from '../components/AddButton'
+import { createConsumer } from 'actioncable'
+
 
 const NotesPage = () => {
 
   let [notes, setNotes] = useState([]);
 
   useEffect(() => {
-    getNotes()
+    // getNotes()
+    getNotesChannel()
   }, [])
+
+  const getNotesChannel = () => {
+    const cable = createConsumer('https://rails-vxml.onrender.com/api/v1/notes/cable'); // Substitua pela URL correta
+
+    const noteChannel = cable.subscriptions.create(
+      { channel: 'NoteChannel' },
+      {
+        received: (data) => {
+          if (data.type === 'index') {
+            setNotes(data.notes);
+          } else if (data.type === 'update') {
+            const updatedNote = data.note;
+            setNotes((prevNotes) =>
+              prevNotes.map((note) => (note.id === updatedNote.id ? updatedNote : note))
+            );
+          }
+        },
+      }
+    );
+  }
 
   const getNotes = async () => {
     let response = await fetch('https://rails-vxml.onrender.com/api/v1/notes/')
